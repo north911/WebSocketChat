a490c419356615d4f2a4aabfbbd5a7cb92354064
Andrei Danilenko <north50911@gmail.com>
2018-03-11 22:44:12 +0300

    fixed nullpointers, it works quite better.

--- src/main/java/clientWeb/ChatEndpoint.java
+++ src/main/java/clientWeb/ChatEndpoint.java
@@ -2,9 +2,6 @@ package clientWeb;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
-
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -17,54 +14,50 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/chat/{username}/{userrole}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
 public class ChatEndpoint {
 
-    private ChatUtils chatUtils = new ChatUtils();
+
     private Session session;
     private ChatUser chatUser;
-    //private static final Set<ChatEndpoint> SERVER_ENDPOINTS = new CopyOnWriteArraySet<>();
     private static HashMap<String, ChatUser> users = new HashMap<>();
+    private ChatUtils chatUtils = new ChatUtils(users);
 
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username, @PathParam("userrole") String userrole) throws IOException, EncodeException {
 
         this.session = session;
-        //SERVER_ENDPOINTS.add(this);
         chatUser = chatUtils.registreChatUser(username, userrole, session);
         users.put(session.getId(), chatUser);
         Message message = new Message();
         message.setFrom(username);
         message.setContent("Connected!");
-        sendMessage(message, chatUser);
+        chatUtils.sendMessage(message, chatUser);
     }
 
     @OnMessage
     public void onMessage(Session session, Message message) throws IOException, EncodeException {
         message.setFrom(users.get(session.getId()).getName());
-        sendMessage(message,chatUser);
+        chatUtils.sendMessage(message, users.get(session.getId()));
+        if(users.get(session.getId()).getUserToSession()==null && users.get(session.getId()).getRole().equals("client")){
+            chatUtils.tryAssignAgent(users,users.get(session.getId()));
+        }
+        if(users.get(session.getId()).getUserToSession()!= null)
+            chatUtils.sendMessage(message,users.get(users.get(session.getId()).getUserToSession().getId()));
+        else{
+            message.setFrom("server");
+            message.setContent("нет собеседника");
+            chatUtils.sendMessage(message, users.get(session.getId()));
+        }
     }
 
     @OnClose
     public void onClose(Session session) throws IOException, EncodeException {
-       // SERVER_ENDPOINTS.remove(this);
-        Message message = new Message();
-        message.setFrom(users.get(session.getId()).getName());
-        message.setContent("Disconnected!");
-        sendMessage(message,users.get(session.getId()));
+        ChatUser user = users.get(session.getId());
+        chatUtils.disconnectUsers(user);
+        users.remove(session.getId());
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
         // Do error handling here
-    }
-
-    private void sendMessage(Message message, ChatUser chatUser) throws IOException, EncodeException{
-        chatUser.getSession().getBasicRemote().sendObject(message);
-        if(chatUser.getUserToSession()==null && chatUser.getRole().equals("client")) {
-            ChatUser agent = chatUtils.findAvailableAgent(users);
-            agent.getSession().getBasicRemote().sendObject(message);
-            if(agent!=null)
-                agent.getSession().getBasicRemote().sendObject(message);
-        }
-
     }
 
 }
--- src/main/java/clientWeb/ChatUtils.java
+++ src/main/java/clientWeb/ChatUtils.java
@@ -1,30 +1,67 @@
 package clientWeb;
 
+import javax.websocket.EncodeException;
 import javax.websocket.Session;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class ChatUtils {
 
-    public ChatUser registreChatUser(String name, String role, Session session){
+    private HashMap<String, ChatUser> users;
+
+    public ChatUtils(HashMap<String, ChatUser> users){
+        this.users = users;
+    }
+
+    public ChatUser registreChatUser(String name, String role, Session session) {
         ChatUser chatUser = new ChatUser();
         chatUser.setName(name);
         chatUser.setSession(session);
-        if("agent".equals(role)){
+        if ("agent".equals(role)) {
             chatUser.setAvailable(true);
             chatUser.setRole("agent");
-        }
-        else{
+        } else {
             chatUser.setAvailable(false);
             chatUser.setRole("client");
         }
         return chatUser;
     }
 
-    public ChatUser findAvailableAgent(HashMap<String, ChatUser> users){
+    private ChatUser findAvailableAgent(HashMap<String, ChatUser> users) {
         for (ChatUser chatUser : users.values()) {
-            if(chatUser.isAvailable()&&"agent".equals(chatUser.getRole()))
+            if (chatUser.isAvailable() && "agent".equals(chatUser.getRole()))
                 return chatUser;
         }
         return null;
+    }
+
+    public boolean tryAssignAgent(HashMap<String, ChatUser> users, ChatUser chatUser) {
+
+        ChatUser agent = findAvailableAgent(users);
+        if (agent != null) {
+            agent.setAvailable(false);
+            agent.setUserToSession(chatUser.getSession());
+            chatUser.setUserToSession(agent.getSession());
+            return true;
+        }
+        return false;
+    }
+
+    public void disconnectUsers(ChatUser chatUser) throws IOException, EncodeException{
+        Message message = new Message();
+        message.setFrom(chatUser.getName());
+        message.setContent("Disconnected!");
+        //sendMessage(message, chatUser);
+        if(chatUser.getUserToSession()!=null){
+            sendMessage(message, users.get(chatUser.getUserToSession().getId()));
+            users.get(chatUser.getUserToSession().getId()).setUserToSession(null);
+            users.get(chatUser.getUserToSession().getId()).setAvailable(true);
+        }
+
+    }
+
+    public void sendMessage(Message message, ChatUser chatUser) throws IOException, EncodeException {
+        chatUser.getSession().getBasicRemote().sendObject(message);
+
     }
 }
